0cbc6be7646f8d0653594e19714212d162797bc0
andrey.piekas <andrey.piekas@CHEINFD03.alunos.sc.senac.br>
2021-10-21 19:38:09 -0300

    Inicio do repositório externo.

--- src/nextlevelpiekasoficial/NextLevel.java
+++ src/nextlevelpiekasoficial/NextLevel.java
@@ -16,6 +16,8 @@ public class NextLevel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Teste");
+        
     }
     
 }
