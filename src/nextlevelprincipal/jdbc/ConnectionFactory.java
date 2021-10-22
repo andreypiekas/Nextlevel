19de819cd933c18156d1a1b8a73c574d59b9492a
andrey.piekas <andrey.piekas@CHEINFD03.alunos.sc.senac.br>
2021-10-21 21:26:52 -0300

    Inicio do repositório externo.

--- src/nextlevelprincipal/jdbc/ConnectionFactory.java
+++ src/nextlevelprincipal/jdbc/ConnectionFactory.java
@@ -4,18 +4,30 @@
  * and open the template in the editor.
  */
 package nextlevelprincipal.jdbc;
+import java.sql.DriverManager;   
+import java.sql.SQLException;
 import java.sql.Connection;
+
 
 /**
  *
  * @author andrey.piekas
  */
 public class ConnectionFactory {
-    public Connection getConnection{
+    public static Connection getConnection(){
+        final String url = "jdbc:mysql://localhost:3306/nextlevelpiekas?verifyServerCertificate=false&useSSL=true";
+        final String user = "andreypiekas";
+        final String password = "piekas123";
+       
             try {
+               // conn = 
+                DriverManager.getConnection(url, user, password);
+            } catch (SQLException ex) {
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
+            } 
+        return null;
                 
-            } catch (Exception e) {
-            }
-}
-    
+}   
 }
