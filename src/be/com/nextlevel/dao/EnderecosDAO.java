9a934092ae4fe8fcb0c8dbf941fb0cf464baeef2
andrey.piekas <andrey.piekas@CHEINFD03.alunos.sc.senac.br>
2021-10-26 21:18:22 -0300

    Inicio do repositório externo.

---
+++ src/be/com/nextlevel/dao/EnderecosDAO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.com.nextlevel.dao;
+
+import br.com.nextlevel.jdbc.ConnectionFactory;
+import java.sql.*;
+
+
+/**
+ *
+ * @author andrey.piekas
+ */
+public class EnderecosDAO {
+    
+    ///Construtor
+    EnderecosDAO enderecos = new EnderecosDAO();
+    public EnderecosDAO(){
+        
+        
+    }
+    
+    
+}
