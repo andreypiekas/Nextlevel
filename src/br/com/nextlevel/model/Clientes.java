62f87024af9623166671b9d3e58693217bcbaa89
andrey.piekas <andrey.piekas@CHEINFD03.alunos.sc.senac.br>
2021-10-26 21:08:48 -0300

    Inicio do repositório externo.

--- src/br/com/nextlevel/model/Clientes.java
+++ src/br/com/nextlevel/model/Clientes.java
@@ -18,86 +18,51 @@ public class Clientes {
     private String telefone;
     private Enderecos endereco;
 
-    /**
-     * @return the id
-     */
+    //getters e setters
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the nome
-     */
     public String getNome() {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /**
-     * @return the email
-     */
     public String getEmail() {
         return email;
     }
 
-    /**
-     * @param email the email to set
-     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * @return the cpf
-     */
     public String getCpf() {
         return cpf;
     }
 
-    /**
-     * @param cpf the cpf to set
-     */
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
-    /**
-     * @return the telefone
-     */
     public String getTelefone() {
         return telefone;
     }
 
-    /**
-     * @param telefone the telefone to set
-     */
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
 
-    /**
-     * @return the endereco
-     */
     public Enderecos getEndereco() {
         return endereco;
     }
 
-    /**
-     * @param endereco the endereco to set
-     */
     public void setEndereco(Enderecos endereco) {
         this.endereco = endereco;
     }
--- src/br/com/nextlevel/model/Enderecos.java
+++ src/br/com/nextlevel/model/Enderecos.java
@@ -20,118 +20,74 @@ public class Enderecos {
     private String bairro;
     private String cidade;
     private String estado;
+    
+    //getters e setters
 
-    /**
-     * @return the idEndereco_entrega
-     */
     public int getIdEndereco_entrega() {
         return idEndereco_entrega;
     }
 
-    /**
-     * @param idEndereco_entrega the idEndereco_entrega to set
-     */
     public void setIdEndereco_entrega(int idEndereco_entrega) {
         this.idEndereco_entrega = idEndereco_entrega;
     }
 
-    /**
-     * @return the cep
-     */
     public String getCep() {
         return cep;
     }
 
-    /**
-     * @param cep the cep to set
-     */
     public void setCep(String cep) {
         this.cep = cep;
     }
 
-    /**
-     * @return the rua
-     */
     public String getRua() {
         return rua;
     }
 
-    /**
-     * @param rua the rua to set
-     */
     public void setRua(String rua) {
         this.rua = rua;
     }
 
-    /**
-     * @return the numero
-     */
     public int getNumero() {
         return numero;
     }
 
-    /**
-     * @param numero the numero to set
-     */
     public void setNumero(int numero) {
         this.numero = numero;
     }
 
-    /**
-     * @return the complemento
-     */
     public String getComplemento() {
         return complemento;
     }
 
-    /**
-     * @param complemento the complemento to set
-     */
     public void setComplemento(String complemento) {
         this.complemento = complemento;
     }
 
-    /**
-     * @return the bairro
-     */
     public String getBairro() {
         return bairro;
     }
 
-    /**
-     * @param bairro the bairro to set
-     */
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
 
-    /**
-     * @return the cidade
-     */
     public String getCidade() {
         return cidade;
     }
 
-    /**
-     * @param cidade the cidade to set
-     */
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
 
-    /**
-     * @return the estado
-     */
     public String getEstado() {
         return estado;
     }
 
-    /**
-     * @param estado the estado to set
-     */
     public void setEstado(String estado) {
         this.estado = estado;
     }
+    
 
+   
 
 }
