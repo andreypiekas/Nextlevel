f99f6ced7e3f749c72c309caf27fce6d353e1f38
andrey.piekas <andrey.piekas@CHEINFD03.alunos.sc.senac.br>
2021-10-26 21:33:17 -0300

    Inicio do repositório externo.

--- src/be/com/nextlevel/dao/EnderecosDAO.java
+++ src/be/com/nextlevel/dao/EnderecosDAO.java
@@ -5,22 +5,31 @@
  */
 package be.com.nextlevel.dao;
 
-import br.com.nextlevel.jdbc.ConnectionFactory;
+import br.com.nextlevel.jdbc.FabricaConexao;
 import java.sql.*;
-
+import br.com.nextlevel.model.Enderecos;
 
 /**
  *
  * @author andrey.piekas
  */
 public class EnderecosDAO {
-    
+
+    private Connection conexao;
     ///Construtor
     EnderecosDAO enderecos = new EnderecosDAO();
-    public EnderecosDAO(){
-        
+
+    public EnderecosDAO() {
+        //Garante que uma conexao esteja aberta com o banco de dados
+        this.conexao = FabricaConexao.criaConexao();
+
+    }
+
+    //metodos
+    //metodos para cadastrar os enderecos
+    
+    public void cadastrarEndereco(Enderecos obj){
         
     }
-    
     
 }
